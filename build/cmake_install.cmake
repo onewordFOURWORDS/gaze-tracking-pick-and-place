247ad79fcdf5e084f1cbef718cddba939fc2e7f9
onewordFOURWORDS <timdanham@gmail.com>
2022-06-22 08:52:55 -0400

    Created a object detection package

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -132,6 +132,7 @@ if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
   include("/home/tim/catkin_ws/build/gtest/cmake_install.cmake")
   include("/home/tim/catkin_ws/build/gaze_tracking/cmake_install.cmake")
+  include("/home/tim/catkin_ws/build/object_detection/cmake_install.cmake")
 
 endif()
 
--- build/object_detection/CTestTestfile.cmake
+++ build/object_detection/CTestTestfile.cmake
@@ -1,9 +1,6 @@
 # CMake generated Testfile for 
-# Source directory: /home/tim/catkin_ws/src
-# Build directory: /home/tim/catkin_ws/build
+# Source directory: /home/tim/catkin_ws/src/object_detection
+# Build directory: /home/tim/catkin_ws/build/object_detection
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("gaze_tracking")
-subdirs("object_detection")
