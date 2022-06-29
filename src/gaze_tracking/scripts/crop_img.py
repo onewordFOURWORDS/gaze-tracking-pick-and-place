38379bc37e0bbb677a1698b893aa5c3fc7e81d9f
onewordFOURWORDS <timdanham@gmail.com>
2022-06-29 10:30:36 -0400

    node to crop the image to standard size

--- src/gaze_tracking/scripts/crop_img.py
+++ src/gaze_tracking/scripts/crop_img.py
@@ -4,6 +4,9 @@ import numpy as np
 import cv2 as cv
 from cv_bridge import CvBridge
 from sensor_msgs.msg import Image
+from sensor_msgs.msg import CameraInfo
+
+img_received = False
 
 # get the image message
 def get_image(ros_img):
@@ -14,15 +17,55 @@ def get_image(ros_img):
 	# raise flag
 	img_received = True
 
+def get_info(d):
+    global height
+    global width
+
+    height = d.height
+    width = d.width
+
+
+
 def main():
+    rospy.init_node('crop_img', anonymous = True)
     # subscribe to the camera image that we want to crop
     img_sub = rospy.Subscriber("/camera/color/image_raw", Image, get_image)
     # Get camera info to dynamically crop the camera resolution
-    info_sub = rospy.Subscriber("/camera/color/camera_info")
+    info_sub = rospy.Subscriber("/camera/color/camera_info", CameraInfo, get_info)
 
     # initlize publisher to publish the cropped image
     img_pub = rospy.Publisher('/cropped_img', Image, queue_size = 1)
 
+    # set rospy rate
     rate = rospy.Rate(10)
-	while not rospy.is_shutdown():
+
+    while not rospy.is_shutdown():
         if img_received:
+            # The monitor that gaze tracking is linked to is 1280x1024
+            # 5:4 resolution
+            # crop image to match a 5:4 resolution
+
+            # Use 1/5 the height * 4 to get proper 5:4 ratio
+            height_aspect = height//5
+            width_aspect = height_aspect*4
+
+            # maths stuff for the actual cropping bit
+            remove = width-width_aspect
+            # remove equal amounts from left and right
+            left = remove//2
+            right = remove//2 - width
+
+            # Keep full height, remove equal amounts from left and right side of image
+            cropped_img = rgb_img[0:height, left:right]
+
+            cropped_img = rgb_img[0:720, 0:900]
+
+            # convert cropped image to image msg
+            img_msg = CvBridge().cv2_to_imgmsg(cropped_img, encoding="rgb8")
+            # publish img
+            img_pub.publish(img_msg)
+
+        rate.sleep()
+
+if __name__ == '__main__':
+	main()
